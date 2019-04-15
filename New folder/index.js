419d3bce7fc95540acf58f8c8c3bc92945888883
Sixolele <1122277@students.wits.ac.za>
2019-04-15 22:40:31 +0200

    Commit from lab2

---
+++ New folder/index.js
@@ -0,0 +1,11 @@
+'use strict'
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require('./mainRouter');
+   
+app.use(mainRouter)
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
+
