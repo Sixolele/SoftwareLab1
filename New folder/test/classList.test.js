576fd7881bd7e9f6e27137bf89d86561604b4983
Sixolele <1122277@students.wits.ac.za>
2019-04-20 01:27:20 +0200

    tested get function

--- New folder/test/classList.test.js
+++ New folder/test/classList.test.js
@@ -20,9 +20,16 @@ test('Add: Add should successfully add a new student to list',() =>
     let student = {name:'sixolele',studentNumber: 1122277,yearOfStudy: 4, electives:[{courseCode: 'ELEN4010',yearOffered:4}]}
     
     classList.add(student);
-    let resultantClassList = classList.getList()
-
-    //expect(1).toEqual(1)
+    let resultantClassList = classList.getList();
 
     expect(resultantClassList).toContain(student)
+})
+test('get: get sould correctly return the student at the specified index', () =>
+{
+    let classList = require('../classList')
+    let student = {name:'sixolele',studentNumber: 1122277,yearOfStudy: 4, electives:[{courseCode: 'ELEN4010',yearOffered:4}]}
+    
+    classList.add(student);
+
+    expect(student).toEqual(classList.get(0)) 
 })
